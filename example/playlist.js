3cf421770f2b5e52aeadcaf45c373ad2d1bcf775
duriku <kucera.duri@gmail.com>
2021-12-11 21:51:18 +0000

    initial

---
+++ example/playlist.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    src: "https://primeservice.s3.eu-west-1.amazonaws.com/vidi1.mp4",
+    label: "Video 1"
+  },
+  {
+    src: "https://primeservice.s3.eu-west-1.amazonaws.com/vidi2.mp4",
+    label: "Video 2"
+  },
+  {
+    src: "https://primeservice.s3.eu-west-1.amazonaws.com/vidi3.mp4",
+    label: "Video 3 (Premium)"
+  },
+  {
+    src: "https://primeservice.s3.eu-west-1.amazonaws.com/vidi4.mp4",
+    label: "Video 4 (Premium)"
+  },
+  {
+    src: "https://primeservice.s3.eu-west-1.amazonaws.com/vidi5.mp4",
+    label: "Video 5 (Premium)"
+  },
+];
